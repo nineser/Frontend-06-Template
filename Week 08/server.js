b6d67defddcb473a23cabdd37ce4ed9a823cb0b4
laijunjie <wo@laijunjie.com>
2020-12-21 10:35:00 +0800

    finish http require and parse response

--- Week 08/server.js
+++ Week 08/server.js
@@ -4,9 +4,25 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("hello http");
+  let body = [];
+  req
+    .on("error", () => {
+      console.log(err);
+    })
+    .on("data", (chunk) => {
+      //转为编码后的字符串的格式
+      body.push(chunk.toString());
+    })
+    .on("end", () => {
+      console.log(body);
+      body = body.join("");
+      //console.log("body:", body);
+      //单独设置时，返回的报文，不包含transfer-encoding 字段
+      //res.statusCode = 200;
+      //res.setHeader("Content-Type", "text/html");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(" hello http\n");
+    });
 });
 
 server.listen(port, hostname, () => {
